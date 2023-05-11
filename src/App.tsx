7e5b0a8e7b9da52fb3b7ef6d8155d91f30f571b2
ductc241 <tacongduc123@gmail.com>
2023-05-11 10:55:48 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="calendar-contaier">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aspernatur repellat
+          facere, ducimus magni doloribus culpa quo aliquid harum cupiditate, corrupti quasi.
+          Facilis odit iure maxime harum quidem deserunt maiores?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
